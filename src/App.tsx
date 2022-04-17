d0b1e23e08e9231469c06b5faf04e7a2c9a19e4c
jasbel <asbel.elmec@gmail.com>
2022-04-17 18:45:52 -0400

    feat: add chakra ui.

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,19 @@ import './App.css'
 import Home from './pages/Home'
 import Header from './layout/Header'
 import Footer from './layout/Footer'
+import { ChakraProvider } from '@chakra-ui/react'
 
 function App() {
   
 
   return (
+    <ChakraProvider>
     <div className="App">
         <Header />
         <Home />
         <Footer />
-      
     </div>
+    </ChakraProvider>
   )
 }
 
